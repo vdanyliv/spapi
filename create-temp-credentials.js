478deaba0b3c7f7f63e1ebfd78c12a390a082611
Volodymyr <vdanyliv.work@gmail.com>
2020-12-19 21:08:08 +0200

    Create create-temp-credentials.js

---
+++ create-temp-credentials.js
@@ -0,0 +1,18 @@
+const createTemporaryAWSCredentials = async() => {
+	const {
+		Credentials: {
+			AccessKeyId,
+			SecretAccessKey,
+			SessionToken,
+		},
+	} = await STS.assumeRole({
+		RoleArn: process.env.SP_ROLE_ARN, // pass role arn that you used while creating your amazon seller app
+		RoleSessionName: 'sp-api',
+	}).promise();
+
+	return {
+		AccessKeyId,
+		SecretAccessKey,
+		SessionToken,
+	};
+};
