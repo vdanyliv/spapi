d3f1b97ab29d14adb87ce5be48591d7de8eb8303
Volodymyr <vdanyliv.work@gmail.com>
2020-12-19 21:37:38 +0200

    Create request-config.js

---
+++ request-config.js
@@ -0,0 +1,28 @@
+const {regions} = require('./regions');
+
+const requestConfig = (regionId, access_token, tempCreds) => {
+		const {
+			AccessKeyId,
+			SessionToken,
+      SecretAccessKey,
+		} = tempCreds;
+
+		const {
+			aws_region,
+			end_point,
+		} = regions.get(regionId);
+
+		const params = {
+			path: '/catalog/v0/items?find=toys',
+			method: 'GET',
+			host: end_point,
+			region: aws_region,
+			service: 'execute-api',
+			headers: {
+				'User-Agent': 'MyAmazonApp/1.0 (Language=JavaScript;)',
+				'x-amz-access-token': access_token,
+			},
+		};
+
+		return aws4.sign(params, {AccessKeyId, SecretAccessKey, SessionToken});
+	}
