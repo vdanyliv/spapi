77cf52b0fa8d86b48776eb619fea40cfe91f60cd
Volodymyr <vdanyliv.work@gmail.com>
2020-12-19 22:16:41 +0200

    Update request.js

--- request.js
+++ request.js
@@ -1,1 +1,19 @@
+const request = signedRequest => {
+    return new Promise((resolve, reject) => {
+      https.request(signedRequest, (res) => {
+        let data = '';
+        const {statusCode} = res;
 
+        res.on('data', chunk => (data += chunk));
+        res.on('end', async() => {
+          const res = data && JSON.parse(data);
+
+          if (statusCode === 200) {
+            resolve(res);
+          } else {
+            reject({statusCode, res});
+          }
+        });
+      }).end();
+    });
+};
